510e2aaaa8c6bb62bea8e71161c28a9cad5f5c8f
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-09-10 21:38:33 +0500

    modified for responsive

--- src/App.js
+++ src/App.js
@@ -32,10 +32,10 @@ function App() {
       <div className="mb-150px">
         <Team />
       </div>
-      {/* <div className="mb-200px">
+      <div className="mb-200px">
         <JoinPrivateSale />
       </div>
-      <Footer /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/layouts/JoinPrivateSale.js
+++ src/layouts/JoinPrivateSale.js
@@ -1,15 +1,25 @@
+import Title from "components/Title";
+import useMediaQuery from "hooks/useMediaQuery";
 import React from "react";
 
 function JoinPrivateSale() {
-  return (
-    <div>
-      <div className="container">
-        <div className="join-private-sales max-w-[800px] w-full rounded-2xl mx-auto py-16 px-9 flex flex-col items-center text-center">
-          <h1 className="text-center text-5xl text-white font-medium mb-10 capitalize">
-            Join the private <span className="text-green">Sale</span>
-          </h1>
+  const isAbove640px = useMediaQuery("(min-width : 640px)");
 
-          <button className="gradient-button py-4 px-8 text-base">
+  return (
+    <div className={isAbove640px ? null : "join-private-sales"}>
+      <div className="container">
+        <div
+          className={`${
+            isAbove640px ? "join-private-sales" : null
+          } max-w-[500px] xl:max-w-[800px] w-full rounded-2xl mx-auto py-12 xl:py-16 sm:px-6 md:px-9 flex flex-col items-center text-center`}
+        >
+          <div className="mb-8 xl:mb-10">
+            <Title>
+              Join the private <span className="text-green">Sale</span>
+            </Title>
+          </div>
+
+          <button className="gradient-button py-3 xl:py-4 px-6 xl:px-8 text-xs xl:text-base">
             CONNECT WALLET
           </button>
         </div>
